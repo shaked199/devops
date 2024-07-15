140b6e2662e1996778ea388794155dabd2fc92cc
shaked199 <saked199@gmail.com>
2024-07-15 21:08:29 +0300

    Add files via upload
    upload files

---
+++ project.js
@@ -0,0 +1,20 @@
+
+<html>
+<body>
+
+<h2>My First JavaScript</h2>
+
+<button type="button"
+onclick="document.getElementById('demo').innerHTML = Date()">
+Click me to display Date and Time.</button>
+
+<p id="demo"></p>
+
+</body>
+</html> 
+console.obj
+{
+    "shaked"
+    
+}
+console.log(obj.null);
